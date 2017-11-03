d9b8326815a20c31e87f3f01c827c8104f28da38
Teo Klestrup Röijezon <teo@nullable.se>
2017-11-03 15:38:40 +0100

    Test win states

--- server/src/test/scala/model/GameSpec.scala
+++ server/src/test/scala/model/GameSpec.scala
@@ -35,5 +35,32 @@ class GameSpec extends WordSpec with Matchers {
         game.clue shouldEqual "a_a"
       }
     }
+
+    "having lost the game" should {
+      "not allow any more tries" in {
+        val game = new Game("z")
+        var letter = 'a'
+        while (!game.gameOver) {
+          game.tryLetter(letter)
+          letter = (letter.toInt + 1).toChar
+        }
+        game.gameOver shouldEqual true
+        game.isSolved shouldEqual false
+        game.tryLetter('z')
+        game.gameOver shouldEqual true
+        game.isSolved shouldEqual false
+      }
+    }
+
+    "having solved the game" should {
+      "be won" in {
+        val game = new Game("z")
+        game.gameOver shouldEqual false
+        game.isSolved shouldEqual false
+        game.tryLetter('z')
+        game.gameOver shouldEqual true
+        game.isSolved shouldEqual true
+      }
+    }
   }
 }
