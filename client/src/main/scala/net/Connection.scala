f0bd16433ec0bcc845034ed553f8cb25ed7fce26
Teo Klestrup Röijezon <teo@nullable.se>
2017-11-20 17:30:51 +0100

    Rewrote client to use non-blocking sockets

--- client/src/main/scala/net/Connection.scala
+++ client/src/main/scala/net/Connection.scala
@@ -1,42 +1,98 @@
 package se.nullable.kth.id1212.hangman.client.net
 
-import java.io.EOFException
-import java.net.{ InetSocketAddress, Socket }
-import se.nullable.kth.id1212.hangman.proto.{ Packet, PacketReader, PacketWriter }
+import java.net.InetSocketAddress
+import java.nio.channels.{ ClosedChannelException, SelectionKey, Selector, SocketChannel }
+
+import scala.collection.JavaConverters._
+import se.nullable.kth.id1212.hangman.proto.{AsyncPacketReader, AsyncPacketWriter, Packet}
 
 class Connection(packetListener: Packet => Unit) {
-  private var socket: Socket = new Socket()
-  private var writer: Option[PacketWriter] = None
+  private var socket: SocketChannel = SocketChannel.open()
+  private var thread: Option[ConnectionThread] = None
 
   def start(host: String, port: String): Unit = {
     stop()
-    socket = new Socket()
+    socket = SocketChannel.open()
     socket.connect(new InetSocketAddress(host, port.toInt))
-    writer = Some(new PacketWriter(socket.getOutputStream))
-    val reader = new PacketReader(socket.getInputStream)
-    new ReaderThread(reader, packetListener).start()
+    socket.configureBlocking(false)
+    val t = new ConnectionThread(socket, packetListener)
+    t.start()
+    thread = Some(t)
   }
 
   def stop(): Unit = {
-    socket.close()
+    thread.foreach { t =>
+      t.close()
+      t.join()
+    }
   }
 
   def sendPacket(packet: Packet): Unit = {
-    writer.get.write(packet)
+    thread.get.write(packet)
   }
 }
 
-class ReaderThread(reader: PacketReader, listener: Packet => Unit) extends Thread {
+class ConnectionThread(socket: SocketChannel, listener: Packet => Unit) extends Thread {
   setDaemon(true)
 
+  private val selector = Selector.open()
+  private val socketKey = socket.register(selector, SelectionKey.OP_READ | SelectionKey.OP_WRITE)
+
+  private val reader = new AsyncPacketReader(socket)
+  private val writer = new AsyncPacketWriter(socket)
+
   override def run(): Unit = {
+    while (selector.isOpen()) {
+      selector.select()
+      for (key <- selector.selectedKeys().asScala) {
+        selector.selectedKeys().remove(key)
+        if (key.isReadable()) {
+          read()
+        }
+        if (key.isWritable()) {
+          flush()
+        }
+      }
+    }
+  }
+
+  def read(): Unit = {
     try {
-      while(true) {
-        listener(reader.readNext())
+      var hasMore = true
+      while(hasMore) {
+        reader.readNext() match {
+          case Some(packet) =>
+            listener(packet)
+          case None =>
+            hasMore = false
+        }
       }
     } catch {
-      case _: EOFException =>
+      case _: ClosedChannelException =>
         // Game over, terminate loop
+        close()
     }
+  }
+
+  def write(packet: Packet): Unit = {
+    writer.synchronized {
+      writer.write(packet)
+      socketKey.interestOps(socketKey.interestOps() | SelectionKey.OP_WRITE)
+    }
+  }
+
+  def flush(): Unit = {
+    writer.synchronized {
+      if (!writer.flush()) {
+        socketKey.interestOps(socketKey.interestOps() & ~SelectionKey.OP_WRITE)
+      }
+    }
+  }
+
+  def close(): Unit = {
+    for (key <- selector.keys().asScala) {
+      key.channel().close()
+    }
+    selector.close()
   }
 }
